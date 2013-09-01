319c38f2b3cd0852507ef59ee45c3c4006f3c363
HenriqueLacerda <geraldohenriquelacerda@gmail.com>
2013-09-01 01:09:54 -0300

    Mascaras para campos

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ActiveRecord::Base
   validates_presence_of :precoVenda
   belongs_to :categoryProduct
   belongs_to :mark
-  attr_accessible :categoryProduct_id, :categoryProduct, :descricao, :nome, :observacao, :precoCompra, :precoMedio, :precoVenda, :quantidadeEstoqueMaximo, :quantidadeEstoqueMinimo
+  attr_accessible :categoryProduct_id, :categoryProduct, :mark_id, :mark, :descricao, :nome, :observacao, :precoCompra, :precoMedio, :precoVenda, :quantidadeEstoqueMaximo, :quantidadeEstoqueMinimo
 end
