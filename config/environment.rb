c8aeaee143cfb4ba72007c417b306b5d1295028b
Geraldo Henrique <geraldohenriquelacerda@gmail.com>
2014-12-07 10:32:43 -0200

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ControleManutencaoVenda::Application.initialize!
-CODECLIMATE_REPO_TOKEN=2075f9311b7744cb63c878452b32adc6ba165f554bfbcc98b50029e33e3a2330 bundle exec rake
+#CODECLIMATE_REPO_TOKEN=2075f9311b7744cb63c878452b32adc6ba165f554bfbcc98b50029e33e3a2330 bundle exec rake
